c41bc9d2fbfb0b77a3e92cbb637dd87b6785a447
Anton Miroshkin <anton.miroshkin@gmail.com>
2019-12-17 09:36:11 +0300

    Added project

---
+++ DesignPatterns_Prototype/DesignPatterns_Prototype/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace DesignPatterns_Prototype
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Design Patterns - Prototype!");
+
+        }
+    }
+}
